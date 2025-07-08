3d2dbd475227428236194ce7e8d4a1efe5ccc7f1
GinaRM <ginarm12@hotmail.com>
2025-07-08 11:08:28 -0500

    init project

---
+++ src/main/java/com/gina/servicio_empleado/ServicioEmpleadoApplication.java
@@ -0,0 +1,13 @@
+package com.gina.servicio_empleado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioEmpleadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioEmpleadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gina/servicio_empleado/ServicioEmpleadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gina.servicio_empleado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioEmpleadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
